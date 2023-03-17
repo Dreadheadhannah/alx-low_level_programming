e467fbeafdfd8c570c7930c686f8e8be4bb25439
dreadheadhannah <princessh851@gmail.com>
2023-03-17 10:16:22 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,38 +4,24 @@
  * Return: Always 0 (Succes)
  */
 
-int main(void)
-{
-	int tho;
-	int hun;
-	int ten;
-	int bas;
+#include <stdio.h>
 
-for ( tho = 0 ; tho < 10 ; tho++)
-{	
+int main() {
+    int i, j;
 
-	for (hun = 0 ; hun <= 10 ; hun++)
-	{
-	
-		for (ten = 0 ; ten < 10 ; ten++)
-		{
-			for ( bas = 0 ; bas < 10 ; bas++)
-			{
-				putchar('0' + tho);
-				putchar('0' + hun);
-				putchar(32);
-				putchar('0' + ten);
-				putchar('0' + bas);
-				if (!(tho == 9 && hun == 8))
-				{
-					putchar(',');
-					putchar(32);
-				}
-				bas++;
-			}
-		}
-	}
-}
-	putchar('\n');
-	return (0);
+    for (i = 0; i < 100; i++) {
+        for (j = i; j < 100; j++) {
+            putchar((i / 10) + '0');
+            putchar((i % 10) + '0');
+            putchar(' ');
+            putchar((j / 10) + '0');
+            putchar((j % 10) + '0');
+            if (i != 99 || j != 99) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    return 0;
 }
