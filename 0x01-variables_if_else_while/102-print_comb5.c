27bc23a7b0b29b9b12402fbc08c7c8fd57d8e58c
dreadheadhannah <princessh851@gmail.com>
2023-03-18 04:33:15 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,27 +1,32 @@
-/**
- * main - Entry point
- *
- * Return: Always 0 (Succes)
- */
-
 #include <stdio.h>
 
-int main() {
-    int i, j;
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int num1, num2;
 
-    for (i = 0; i < 100; i++) {
-        for (j = i; j < 100; j++) {
-            putchar((i / 10) + '0');
-            putchar((i % 10) + '0');
-            putchar(' ');
-            putchar((j / 10) + '0');
-            putchar((j % 10) + '0');
-            if (i != 99 || j != 99) {
-                putchar(',');
+    for (num1 = 0; num1 < 100; num1++)
+    {
+        for (num2 = num1; num2 < 100; num2++)
+        {
+            if (num1 != num2)
+            {
+                putchar((num1 / 10) + '0');
+                putchar((num1 % 10) + '0');
                 putchar(' ');
+                putchar((num2 / 10) + '0');
+                putchar((num2 % 10) + '0');
+
+                if (num1 != 98 || num2 != 99)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
             }
         }
     }
-
-    return 0;
+    return (0);
 }
