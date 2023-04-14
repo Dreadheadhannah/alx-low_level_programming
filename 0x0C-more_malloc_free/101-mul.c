7fd6c8d0438f639100db5787d160ed323206df73
dreadheadhannah <princessh851@gmail.com>
2023-04-13 20:14:23 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,38 +1,67 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+#include <ctype.h>
 
-/**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
-**/
+void error_exit() {
+    printf("Error\n");
+    exit(98);
+}
 
-int main(int argc, char *argv[])
-{
-	unsigned long mul;
-	int i, j;
+int main(int argc, char *argv[]) {
+    int i, j, len1, len2, carry, num1, num2;
+    int *result;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+    if (argc != 3) {
+        error_exit();
+    }
 
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
-		}
-	}
-	mul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", mul);
-	return (0);
+    for (i = 1; i <= 2; i++) {
+        for (j = 0; argv[i][j] != '\0'; j++) {
+            if (!isdigit(argv[i][j])) {
+                error_exit();
+            }
+        }
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+
+    if (num1 == 0 || num2 == 0) {
+        printf("0\n");
+        return 0;
+    }
+
+    len1 = sprintf(NULL, "%d", num1);
+    len2 = sprintf(NULL, "%d", num2);
+
+    result = calloc(len1 + len2, sizeof(int));
+
+    for (i = len1 - 1; i >= 0; i--) {
+        carry = 0;
+        for (j = len2 - 1; j >= 0; j--) {
+            result[i + j + 1] += carry + ((num1 % 10) * (num2 % 10));
+            carry = result[i + j + 1] / 10;
+            result[i + j + 1] %= 10;
+            num2 /= 10;
+        }
+        if (carry > 0) {
+            result[i + j + 1] += carry;
+        }
+        num2 = atoi(argv[2]);
+        num1 /= 10;
+    }
+
+    i = 0;
+    while (result[i] == 0) {
+        i++;
+    }
+
+    for (; i < len1 + len2; i++) {
+        printf("%d", result[i]);
+    }
+    printf("\n");
+
+    free(result);
+    return 0;
 }
