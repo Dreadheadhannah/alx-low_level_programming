ff14d3b7ac36b717636d05bced75ee73b77fa1b1
dreadheadhannah <princessh851@gmail.com>
2023-04-14 17:38:26 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,34 +1,53 @@
 #include <stdio.h>
 
 /**
- * main - prints all arguments it receives
- * @argc: input
- * @argv: input
- * Return: Always 0 (Success)
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-	int i;
+    int i;
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-	return (0);
-}i#include <stdio.h>
-/**
- * main - prints all argument it receives
- * @argc: input
- * @argv: input
- * Return: Always 0 (Success)
- */
-int main(int argc, char *argv[])
-{
-	int i;
+    for (i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-	return (0);
+    return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
