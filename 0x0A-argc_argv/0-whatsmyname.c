bdb330b12416dc34946ce7a2bf27298077148afb
dreadheadhannah <princessh851@gmail.com>
2023-04-14 17:07:09 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc, char **argv)
+/**
+ * main - is a function that prints the name of the file
+ * @argc: is an argument counter for char
+ * @argv: is an argument value for char
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
 {
-    char *program_name = argv[0];
-
-    printf("%s\n", program_name);
-
-    return EXIT_SUCCESS;
+	printf("%s\n", argv[argc - argc]);
+	return (0);
 }
